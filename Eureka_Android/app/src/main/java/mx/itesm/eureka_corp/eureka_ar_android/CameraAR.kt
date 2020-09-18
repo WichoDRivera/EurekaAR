64f17c334fc445b9fc8b939a7cc7aeca4facff53
lunerip <A01745995@itesm.mx>
2020-09-17 21:47:17 -0500

    Activities creation

---
+++ Eureka_Android/app/src/main/java/mx/itesm/eureka_corp/eureka_ar_android/CameraAR.kt
@@ -0,0 +1,11 @@
+package mx.itesm.eureka_corp.eureka_ar_android
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+
+class CameraAR : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_camera_a_r)
+    }
+}
