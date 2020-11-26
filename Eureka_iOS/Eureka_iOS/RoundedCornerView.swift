43d24dc1e1940c4eb3c2f2bf3c92aaec851b4e28
lunerip <A01745995@itesm.mx>
2020-11-25 23:29:18 -0600

    Sign In Screen

---
+++ Eureka_iOS/Eureka_iOS/RoundedCornerView.swift
@@ -0,0 +1,42 @@
+//
+//  RoundedCornerView.swift
+//  Eureka_iOS
+//
+//  Created by Luis Enrique Neri on 25/11/20.
+//
+
+import UIKit
+class RoundedCornerView: UIView {
+
+    // if cornerRadius variable is set/changed, change the corner radius of the UIView
+    @IBInspectable var cornerRadius: CGFloat = 0 {
+        didSet {
+            layer.cornerRadius = cornerRadius
+            layer.masksToBounds = cornerRadius > 0
+        }
+    }
+    
+    @IBInspectable var shadowOpacity: Float {
+        get {
+            return layer.shadowOpacity
+        }
+        set {
+            layer.shadowOpacity = newValue
+            layer.shadowColor = UIColor.darkGray.cgColor
+        }
+        }
+
+    @IBInspectable var shadowOffset: CGSize {
+        get {
+            return layer.shadowOffset
+        }
+        set {
+            layer.shadowOffset = newValue
+            layer.shadowColor = UIColor.black.cgColor
+            layer.masksToBounds = false
+        }
+    }
+    
+    
+    
+}
